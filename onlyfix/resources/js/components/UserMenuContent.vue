a6408e34aa0ae1e4be7bd5de2748e4b5e80bc177
Alex Luczi <luczialex@gmail.com>
2026-01-13 00:22:00 +0100

    add language switcher to user menu

--- onlyfix/resources/js/components/UserMenuContent.vue
+++ onlyfix/resources/js/components/UserMenuContent.vue
@@ -5,20 +5,32 @@ import {
     DropdownMenuItem,
     DropdownMenuLabel,
     DropdownMenuSeparator,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
 } from '@/components/ui/dropdown-menu';
 import { logout } from '@/routes';
 import { edit } from '@/routes/profile';
 import type { User } from '@/types';
 import { Link, router } from '@inertiajs/vue3';
-import { LogOut, Settings } from 'lucide-vue-next';
+import { LogOut, Settings, Globe, Check } from 'lucide-vue-next';
+import { ref } from 'vue';
+import { availableLocales, setLocale, getCurrentLocale, type SupportedLocale } from '@/i18n';
 
 interface Props {
     user: User;
 }
 
+const currentLocale = ref<SupportedLocale>(getCurrentLocale());
+
 const handleLogout = () => {
     router.flushAll();
 };
+
+function changeLocale(locale: SupportedLocale) {
+    setLocale(locale);
+    currentLocale.value = locale;
+}
 
 defineProps<Props>();
 </script>
@@ -34,9 +46,28 @@ defineProps<Props>();
         <DropdownMenuItem :as-child="true">
             <Link class="block w-full" :href="edit()" prefetch as="button">
                 <Settings class="mr-2 h-4 w-4" />
-                Settings
+                {{ $t('nav.settings') }}
             </Link>
         </DropdownMenuItem>
+        <DropdownMenuSub>
+            <DropdownMenuSubTrigger>
+                <Globe class="mr-2 h-4 w-4" />
+                {{ $t('language.title') }}
+            </DropdownMenuSubTrigger>
+            <DropdownMenuSubContent>
+                <DropdownMenuItem
+                    v-for="locale in availableLocales"
+                    :key="locale.code"
+                    @click="changeLocale(locale.code)"
+                >
+                    <span class="flex items-center gap-2 w-full">
+                        <span class="text-base">{{ locale.code === 'en' ? '🇬🇧' : '🇭🇺' }}</span>
+                        <span>{{ locale.nativeName }}</span>
+                        <Check v-if="currentLocale === locale.code" class="ml-auto h-4 w-4" />
+                    </span>
+                </DropdownMenuItem>
+            </DropdownMenuSubContent>
+        </DropdownMenuSub>
     </DropdownMenuGroup>
     <DropdownMenuSeparator />
     <DropdownMenuItem :as-child="true">
@@ -48,7 +79,7 @@ defineProps<Props>();
             data-test="logout-button"
         >
             <LogOut class="mr-2 h-4 w-4" />
-            Log out
+            {{ $t('nav.logout') }}
         </Link>
     </DropdownMenuItem>
 </template>
